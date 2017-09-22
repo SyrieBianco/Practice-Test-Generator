a3e5facfe6c3c037eec05c258833fc2917ef7459
Syrie Bianco <syriebianco@gmail.com>
2017-09-21 22:16:05 -0700

    Add practice problem solutions

--- practice_test.rb
+++ practice_test.rb
@@ -2,6 +2,7 @@
 # Problem 1: Base Converter
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a recursive method that takes in a base 10 number n and
 # converts it to a base b number. Return the new number as a string
@@ -10,7 +11,10 @@
 # base_converter(31, 16) == "1f"
 
 def base_converter(num, b)
+  digits = ('0'..'9').to_a + ('a'..'z').to_a + ('A'..'Z').to_a
+  return digits[num] if num < b
 
+  base_converter(num / b, b) + digits[num % b]
 end
 
 
@@ -21,13 +25,27 @@ end
 # Problem 2: Binary Search
 #______________________________________________________________________
 
+# # # be rspec spec.rb:15 spec.rb:19 spec.rb:27 spec.rb:31 spec.rb:35 spec.rb:39
 
 class Array
 
   # Write a monkey patch of binary search:
   # E.g. [1, 2, 3, 4, 5, 7].my_bsearch(5) => 4
   def my_bsearch(target, &prc)
+    return nil if empty?
+    prc ||= Proc.new { |a, b| a <=> b }
+    mid_i = length / 2
+    left, middle, right = self[0...mid_i], self[mid_i], self[mid_i + 1..-1]
 
+    case prc.call(target, middle)
+    when 0
+      return mid_i
+    when -1
+      return left.my_bsearch(target, &prc)
+    when 1
+      result = right.my_bsearch(target, &prc)
+      result ? result + mid_i + 1 : nil
+    end
   end
 
 end
@@ -40,6 +58,7 @@ end
 # Problem 3: Caesar Cipher
 #______________________________________________________________________
 
+# # # be rspec spec.rb:46 spec.rb:50 spec.rb:54
 
 # Back in the good old days, you used to be able to write a darn near
 # uncrackable code by simply taking each letter of a message and incrementing it
@@ -52,7 +71,21 @@ end
 # the position of a letter in the array, you may use `Array#find_index`.
 
 def caesar_cipher(str, shift)
+  letters = ('a'..'z').to_a
+  ciphered = ""
 
+  str.chars.each do |ch|
+    index = letters.index(ch.downcase)
+    if index
+      new_i = (shift + letters.index(ch.downcase)) % letters.length
+      new_ch = letters[new_i]
+      new_ch.upcase! unless ch == ch.downcase
+    else
+      new_ch = ch
+    end
+    ciphered << new_ch
+  end
+  ciphered
 end
 
 
@@ -63,12 +96,15 @@ end
 # Problem 4: Deep Dup
 #______________________________________________________________________
 
+# # # be rspec spec.rb:67 spec.rb:71
 
 # Using recursion and the is_a? method,
 # write an Array#deep_dup method that will perform a "deep" duplication of the interior arrays.
 
 def deep_dup(arr)
-
+  arr.map do |el|
+    el.is_a?(Array) ? deep_dup(el) : el
+  end
 end
 
 
@@ -79,6 +115,7 @@ end
 # Problem 5: Digital Root
 #______________________________________________________________________
 
+# # # be rspec spec.rb:78 spec.rb:82 spec.rb:95
 
 # Write a method, `digital_root(num)`. It should Sum the digits of a positive
 # integer. If it is greater than 10, sum the digits of the resulting number.
@@ -89,7 +126,12 @@ end
 # one step of the process.
 
 def digital_root(num)
-
+  return num if num < 10
+  result = num
+  until result < 10
+    result = result % 10 + digital_root(result / 10)
+  end
+  result
 end
 
 
@@ -100,12 +142,12 @@ end
 # Problem 6: Doubler
 #______________________________________________________________________
 
+# # # be rspec spec.rb:95
 
 # Write a method that doubles each element in an array
 def doubler(array)
+  array.map { |e| e * 2 }
 end
-
-
 
 
 
@@ -113,6 +155,7 @@ end
 # Problem 7: Dups
 #______________________________________________________________________
 
+# # # be rspec spec.rb:109 spec.rb:113 spec.rb:117 spec.rb:121
 
 class Array
 
@@ -122,7 +165,14 @@ class Array
   # [1, 3, 4, 3, 0, 3, 0].dups => { 3 => [1, 3, 5], 0 => [4, 6] }
 
   def dups
+    hash, dups = Hash.new { |h, k| h[k] = [] }, {}
+    each_with_index do |el, i|
+      hash[el] << i
+      dups[el] = hash[el] if hash[el].length == 2
+    end
+    dups
   end
+
 end
 
 
@@ -133,6 +183,7 @@ end
 # Problem 8: Eight Queens
 #______________________________________________________________________
 
+# # # be rspec
 
 # CHALLENGE: Eight queens puzzle precursor
 #
@@ -157,11 +208,15 @@ end
 # Problem 9: Exponent
 #______________________________________________________________________
 
+# # # be rspec spec.rb:142 spec.rb:146 spec.rb:150
 
 # return b^n recursively. Your solution should accept negative values
 # for n
 def exponent(b, n)
+  return [1, b][n] if n.abs < 2
+  res = b * exponent(b, n.abs - 1)
 
+  n > 0 ? res : 1.0 / res
 end
 
 
@@ -172,12 +227,16 @@ end
 # Problem 10: Factorials Rec
 #______________________________________________________________________
 
+# # # be rspec spec.rb:157 spec.rb:161 spec.rb:165 spec.rb:170
 
 # Write a recursive method that returns the first "num" factorial numbers.
 # Note that the 1st factorial number is 0!, which equals 1. The 2nd factorial
 # is 1!, the 3rd factorial is 2!, etc.
 
-def factorials_rec(num)
+def factorials_rec(n)
+  return [1, 1][0...n] if n <= 2
+  prev = factorials_rec(n - 1)
+  prev << (n - 1) * prev[-1]
 end
 
 
@@ -188,11 +247,19 @@ end
 # Problem 11: Factors
 #______________________________________________________________________
 
+# # # be rspec spec.rb:179 spec.rb:183
 
 # Write a method that returns the factors of a number in ascending order.
 
-def factors(num)
-
+def factors(n)
+  factors, pairs = [], []
+  1.upto(Math.sqrt(n).floor) do |i|
+    if n % i == 0
+      factors.push(i)
+      pairs.unshift(n / i)
+    end
+  end
+  factors + pairs
 end
 
 
@@ -203,25 +270,25 @@ end
 # Problem 12: Fibs Sum
 #______________________________________________________________________
 
+# # # be rspec spec.rb:190 spec.rb:194 spec.rb:198
 
 # Implement a method that finds the sum of the first n
 # fibonacci numbers recursively. Assume n > 0
 def fibs_sum(n)
-
+  return [0, 1][n] if n < 2
+  fibs_sum(n - 1) + fibs_sum(n-2) + 1
 end
-
-
-
-
 
 #______________________________________________________________________
 # Problem 13: First Even Numbers Sum
 #______________________________________________________________________
 
+# # # be rspec spec.rb:206 spec.rb:210
 
 # return the sum of the first n even numbers recursively. Assume n > 0
 def first_even_numbers_sum(n)
-
+  return 0 if n == 0
+  first_even_numbers_sum(n - 1) + 2 * n
 end
 
 
@@ -232,6 +299,7 @@ end
 # Problem 14: Jumble Sort
 #______________________________________________________________________
 
+# # # be rspec spec.rb:217 spec.rb:221 spec.rb:230
 
 # Jumble sort takes a string and an alphabet. It returns a copy of the string
 # with the letters re-ordered according to their positions in the alphabet. If
@@ -241,7 +309,20 @@ end
 # jumble_sort("hello") => "ehllo"
 # jumble_sort("hello", ['o', 'l', 'h', 'e']) => 'ollhe'
 
-def jumble_sort(str, alphabet = nil)
+def jumble_sort(str, alphabet = [])
+  alphabet += ('a'..'z')
+  result = Array.new(str.length)
+
+  str.chars.each do |ch|
+    index = alphabet.index(ch.downcase)
+    if index
+
+    else
+      result << ch
+      next
+    end
+  end
+
 
 end
 
@@ -253,6 +334,7 @@ end
 # Problem 15: Make Better Change
 #______________________________________________________________________
 
+# # # be rspec spec.rb:237 spec.rb:241 spec.rb:245
 
 # make better change problem from class
 # make_better_change(24, [10,7,1]) should return [10,7,7]
@@ -277,6 +359,7 @@ end
 # Problem 16: Median
 #______________________________________________________________________
 
+# # # be rspec spec.rb:258 spec.rb:262 spec.rb:266
 
 # Write a method that returns the median of elements in an array
 # If the length is even, return the average of the middle two elements
@@ -293,6 +376,7 @@ end
 # Problem 17: Merge Sort
 #______________________________________________________________________
 
+# # # be rspec spec.rb:274 spec.rb:278 spec.rb:282 spec.rb:286 spec.rb:300
 
 class Array
 
@@ -314,6 +398,7 @@ end
 # Problem 18: My All
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -331,6 +416,7 @@ end
 # Problem 19: My Any
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -348,6 +434,7 @@ end
 # Problem 20: My Each
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -358,7 +445,7 @@ class Array
   def my_each_with_index(&prc)
 
   end
-  
+
 end
 
 
@@ -369,6 +456,7 @@ end
 # Problem 21: My Each Hash
 #______________________________________________________________________
 
+# # # be rspec
 
 class Hash
 
@@ -387,6 +475,7 @@ end
 # Problem 22: My Flatten
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -415,6 +504,7 @@ end
 # Problem 23: My Inject
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -433,6 +523,7 @@ end
 # Problem 24: My Join
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -450,6 +541,7 @@ end
 # Problem 25: My Merge
 #______________________________________________________________________
 
+# # # be rspec
 
 class Hash
 
@@ -468,6 +560,7 @@ end
 # Problem 26: My Reject
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -485,6 +578,7 @@ end
 # Problem 27: My Reverse
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -502,6 +596,7 @@ end
 # Problem 28: My Rotate
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -519,6 +614,7 @@ end
 # Problem 29: My Select
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -536,13 +632,14 @@ end
 # Problem 30: My Zip
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
   def my_zip(*arrs)
 
   end
-  
+
 end
 
 
@@ -553,6 +650,7 @@ end
 # Problem 31: Prime Factorization
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a recursive function that returns the prime factorization of
 # a given number. Assume num > 1
@@ -574,6 +672,7 @@ end
 # Problem 32: Primes
 #______________________________________________________________________
 
+# # # be rspec
 
 # primes(num) returns an array of the first "num" primes.
 # You may wish to use an is_prime? helper method.
@@ -592,6 +691,7 @@ end
 # Problem 33: Quicksort
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
 
@@ -599,7 +699,7 @@ class Array
   def my_quick_sort(&prc)
 
   end
-  
+
 end
 
 
@@ -610,6 +710,7 @@ end
 # Problem 34: Real Words In String
 #______________________________________________________________________
 
+# # # be rspec
 
 class String
   # Returns an array of all the subwords of the string that appear in the
@@ -628,6 +729,7 @@ end
 # Problem 35: String Include Key
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a recursive method that takes in a string to search and a key string.
 # Return true if the string contains all of the characters in the key
@@ -647,6 +749,7 @@ end
 # Problem 36: Subsets
 #______________________________________________________________________
 
+# # # be rspec
 
 #returns all subsets of an array
 def subsets(array)
@@ -661,6 +764,7 @@ end
 # Problem 37: Symmetric Substrings
 #______________________________________________________________________
 
+# # # be rspec
 
 class String
 
@@ -680,6 +784,7 @@ end
 # Problem 38: Two Sum
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
   # Write a method, `Array#two_sum`, that finds all pairs of positions where the
@@ -704,6 +809,7 @@ end
 # Problem 39: Rec Sum
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a recursive method that returns the sum of all elements in an array
 def rec_sum(nums)
@@ -717,6 +823,7 @@ end
 # Problem 40: Permutations
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a recursive method that returns all of the permutations of an array
 def permutations(array)
@@ -730,6 +837,7 @@ end
 # Problem 41: Bubble Sort
 #______________________________________________________________________
 
+# # # be rspec
 
 class Array
   def bubble_sort!(&prc)
@@ -747,6 +855,7 @@ end
 # Problem 42: Pig Latinify
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a method that translates a sentence into pig latin. You may want a helper method.
 # 'apple' => 'appleay'
@@ -763,13 +872,9 @@ end
 # Problem 43: Titleize
 #______________________________________________________________________
 
+# # # be rspec
 
 # Write a method that capitalizes each word in a string like a book title
 # Do not capitalize words like 'a', 'and', 'of', 'over' or 'the'
 def titleize(title)
 end
-
-
-
-
-
